8faa01f13a67576599a45b18b323ca26b7c79615
jaxywaxy <jacqui.anker@gmail.com>
2026-04-07 11:30:44 +1200

    fix resource_group main.tf

--- modules/resource_groups/outputs.tf
+++ modules/resource_groups/outputs.tf
@@ -1,10 +1,15 @@
+output "platform_name" {
+  value = azurerm_resource_group.platform.name
+}
+
 output "networking_name" {
   value = azurerm_resource_group.networking.name
 }
 
-output "platform_name" {
-  value = azurerm_resource_group.platform.name
+output "apps_name" {
+  value = azurerm_resource_group.apps.name
 }
+
 output "logging_name" {
   value = azurerm_resource_group.logging.name
 }
