e3dbe58fb70c88be8cb1942fa26df9f6a46c86c1
jaxywaxy <jacqui.anker@gmail.com>
2026-04-07 16:40:10 +1200

    cleaned up branch

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "sttfstate18396"
-    container_name       = "tfstate"
-    key                  = "landingzone.tfstate"
-  }
-}
-
