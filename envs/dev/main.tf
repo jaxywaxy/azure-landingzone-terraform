afa9d72a709e98dbed14e50bf4e0dcf45d8d4946
jaxywaxy <jacqui.anker@gmail.com>
2026-04-11 11:23:09 +1200

    fix: update dev main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -10,8 +10,6 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "sttfstate18396"
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "sttfstate18396"
     container_name       = "tfstate"
     key                  = "dev.tfstate"
   }
@@ -69,7 +67,6 @@ module "logging" {
 module "storage_diagnostics" {
   source = "../../modules/diagnostic_settings"
   resource_name      = module.logging.logs_storage_name
-  resource_type      = "Microsoft.Storage/storageAccounts"
   resource_type      = "Microsoft.Storage/storageAccounts"
   target_resource_id = module.logging.logs_storage_id
   law_id             = module.logging.law_id
