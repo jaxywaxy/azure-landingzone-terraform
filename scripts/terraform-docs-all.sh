2e285448ba57ff6e0f7d10fba32fda77dbd68016
jaxywaxy <jacqui.anker@gmail.com>
2026-04-22 14:41:37 +1200

    FIxed bash script

--- scripts/terraform-docs-all.sh
+++ scripts/terraform-docs-all.sh
@@ -7,7 +7,7 @@ for module in "$ROOT_DIR"/modules/*; do
   if [ -d "$module" ]; then
     if [ -f "$module/README.md" ]; then
       echo "Updating terraform-docs for module: $(basename "$module")"
-      terraform-docs "$ROOT_DIR" "$module"
+      terraform-docs "$module"
     else
       echo "Skipping $(basename "$module") — no README.md found"
     fi
